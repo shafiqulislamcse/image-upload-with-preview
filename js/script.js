6176143f9a8f96a79660fa4aad96956151a97656
Md. Shafiqul Islam <63520438+shafiqulislamcse@users.noreply.github.com>
2020-12-26 14:35:46 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	 var $imageupload = $('.imageupload');
+            $imageupload.imageupload();
+
+
+
+     function previewFile(input){
+        var file = $("input[type=file]").get(0).files[0];
+ 
+        if(file){
+            var reader = new FileReader();
+ 
+            reader.onload = function(){
+                $("#previewImg").attr("src", reader.result);
+            }
+ 
+            reader.readAsDataURL(file);
+        }
+    }
+});
